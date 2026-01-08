a1720ceffaf67b8d282fa55ad981b6cfeeff74ae
Phol2003 <dinodragonza123@gmail.com>
2026-01-08 11:52:48 +0700

    even-data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import React from 'react';
 import Button from './class-components';
 import { Calculator2 } from './Caculator';
+import {EventData1} from './event-data'
 
 function App() {
-return <Calculator2/>
+return <EventData1/>
 }
  
 export default App;
