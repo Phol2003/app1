c65f488f4d7162103a1515201a3fe4657f61d5dc
Phol2003 <dinodragonza123@gmail.com>
2026-01-08 10:04:22 +0700

    app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Content, Footer, Header } from './func-components';
+import Calendar from './class-components';
+ 
+function App() {
+return (
+  <>
+  <Header/>
+    <p>
+        <center>
+          <Calendar />
+        </center>
+  </p>
+  <Content/>
+  <Footer/>
+
+  </>
+  
+)
+ 
+}
+ 
+export default App;
