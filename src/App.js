8a5b2d2868d85cf6e18c7d2519306f3cbe350aeb
Phol2003 <dinodragonza123@gmail.com>
2026-01-15 11:20:09 +0700

    state-fun

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Button from './class-components';
 import { Calculator2 } from './Caculator';
 import EventData2 from './even-data'
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
-return <RefsArray/>
+return <MessageBox/>
 }
  
 export default App;
