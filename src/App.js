7444b700f17978e737af33be28029977d3b82745
Phol2003 <dinodragonza123@gmail.com>
2026-02-19 11:45:24 +0700

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Router2 from './router2'
 import Router3 from './router3'
 import Router4 from './router4'
+import FormText from './form-text'
 export default function App() {
 
-  return <Router3 />
+  return <FormText/>
 
 }
  
