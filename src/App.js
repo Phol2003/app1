3975380f406595e12e5dd3c1ede6ce016aab4f6b
Phol2003 <dinodragonza123@gmail.com>
2026-02-19 10:24:39 +0700

    Routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Router2 from './router2'
+import Router3 from './router3'
+import Router4 from './router4'
 export default function App() {
 
-  return <Router2 />
+  return <Router3 />
 
 }
  
