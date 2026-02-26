ae85c40717617fcb8cde4b61a25fa4f2a118806b
Phol2003 <dinodragonza123@gmail.com>
2026-02-26 10:34:46 +0700

    selectfile

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Router2 from './router2'
 import Router3 from './router3'
 import Router4 from './router4'
 import FormText from './form-text'
+import FormSelectFile from './form-select-file'
 export default function App() {
 
-  return <FormText/>
+  return <FormSelectFile/>
 
 }
  
